07f2f8b472400ff0deb43a5c8f38ed3797ad0f32
Mohamed Amn <mamn2@illinois.edu>
2019-01-28 23:43:05 -0600

    Split tests for specificity, added filter methods/tests

--- src/TVSeries.java
+++ src/TVSeries.java
@@ -1,4 +1,7 @@
-import javafx.scene.control.cell.TreeItemPropertyValueFactory;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.util.*;
 
 /**
  * This class stores information about a TV show.
@@ -6,70 +9,46 @@ import javafx.scene.control.cell.TreeItemPropertyValueFactory;
  */
 public class TVSeries {
 
-    public String name;
+    private String seriesName;
     private String language;
     private String[] genres;
     private String premiereDate;
-    private double ratings;
+    private double averageRating;
     private String networkName;
-    private String summary;
+    private String seriesSummary;
     private TVEpisode[][] episodes;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public TVSeries(JsonObject tvShowJSON) throws NullPointerException {
 
-    public void setLanguage(String language) {
-        this.language = language;
-    }
+        //Initializing fields from information found in JSONObject
+        this.setSeriesName(tvShowJSON.get("name").getAsString());
+        this.setLanguage(tvShowJSON.get("language").getAsString());
+        this.setSeriesSummary(tvShowJSON.get("summary").getAsString());
+        this.setAverageRating(tvShowJSON.get("rating").getAsJsonObject().get("average").getAsDouble());
+        this.setNetworkName(tvShowJSON.get("network").getAsJsonObject().get("name").getAsString());
+        this.setPremiereDate(tvShowJSON.get("premiered").getAsString());
 
-    public void setGenres(String[] genres) {
-        this.genres = genres;
-    }
-
-    public void setPremiereDate(String premiereDate) {
-        this.premiereDate = premiereDate;
-    }
-
-    public void setRatings(double ratings) {
-        this.ratings = ratings;
-    }
-
-    public void setNetworkName(String networkName) {
-        this.networkName = networkName;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public void setEpisodes(TVEpisode[] episodes) {
-
-        if (episodes != null) {
-            int numSeasons = episodes[episodes.length - 1].getSeason();
-
-            int maxEpisodesInSeason = 0;
-            for (int i = 0; i < episodes.length; i++) {
-                if (episodes[i].getNumber() > maxEpisodesInSeason) {
-                    maxEpisodesInSeason = episodes[i].getNumber();
-                }
-            }
-
-            TVEpisode[][] organizedEpisodeArray = new TVEpisode[numSeasons][maxEpisodesInSeason];
-            for (TVEpisode episode: episodes) {
-                organizedEpisodeArray[episode.getSeason() - 1 ][episode.getNumber() - 1] = episode;
-            }
-            this.episodes = organizedEpisodeArray;
+        //Since the genres are stored in a JsonArray, loop through it to convert to a java array
+        String[] genres = new String[tvShowJSON.get("genres").getAsJsonArray().size()];
+        for (int i = 0; i < genres.length; i++) {
+            genres[i] = tvShowJSON.get("genres").getAsJsonArray().get(i).getAsString();
         }
+        this.setGenres(genres);
+
+        //Since the episodes are stored in a JsonArray, we loop through it to convert it to a java array
+        TVEpisode[] episodes = new TVEpisode[tvShowJSON.get("_embedded").getAsJsonObject()
+                .get("episodes").getAsJsonArray().size()];
+        for (int i = 0; i < episodes.length; i++) {
+            episodes[i] = new Gson().fromJson(tvShowJSON.get("_embedded").getAsJsonObject()
+                            .get("episodes").getAsJsonArray().get(i),
+                    TVEpisode.class);
+        }
+        this.setEpisodes(episodes);
 
     }
 
-    public TVEpisode createNewEpisode() {
-        return new TVEpisode();
-    }
-
-    public String getName() {
-        return name;
+    public String getSeriesName() {
+        return seriesName;
     }
 
     public String getLanguage() {
@@ -84,26 +63,179 @@ public class TVSeries {
         return premiereDate;
     }
 
-    public double getRatings() {
-        return ratings;
+    public double getAverageRating() {
+        return averageRating;
     }
 
     public String getNetworkName() {
         return networkName;
     }
 
-    public String getSummary() {
-        return summary;
+    public String getSeriesSummary() {
+        return seriesSummary;
+    }
+
+    public TVEpisode getEpisode(int seasonNum, int episodeNum) throws IllegalArgumentException {
+        if (seasonNum > episodes.length || episodeNum > episodes[seasonNum - 1].length) {
+            throw new IllegalArgumentException();
+        }
+        return episodes[seasonNum - 1][episodeNum - 1];
     }
 
     public TVEpisode[][] getEpisodes() {
         return episodes;
     }
 
-    public TVEpisode getEpisode(int seasonNum, int episodeNum) throws ArrayIndexOutOfBoundsException {
-        return episodes[seasonNum - 1][episodeNum - 1];
+    public TVEpisode getEpisodeOnDate(String date) {
+
+        for (TVEpisode[] season : episodes) {
+            for (TVEpisode episode : season) {
+                if (episode != null && episode.getAirdate().equals(date)) {
+                    return episode;
+                }
+            }
+        }
+        return null;
+
     }
 
+    public void setSeriesName(String seriesName) {
+        this.seriesName = seriesName;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setGenres(String[] genres) {
+        this.genres = genres;
+    }
+
+    public void setPremiereDate(String premiereDate) {
+        this.premiereDate = premiereDate;
+    }
+
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public void setNetworkName(String networkName) {
+        this.networkName = networkName;
+    }
+
+    public void setSeriesSummary(String seriesSummary) {
+        this.seriesSummary = seriesSummary;
+    }
+
+    /**
+     * This functions converts the array of episodes into a 2D array for better readability
+     * @param episodes is all the episodes in a show in the form of a 1D array
+     */
+    public void setEpisodes(TVEpisode[] episodes) throws NullPointerException {
+
+        if (episodes != null) {
+            this.episodes = null;
+
+            //calculates how many seasons there are by going through the episodes and checking their season number
+            int numSeasons = 1;
+            for (TVEpisode episode: episodes) {
+                if (episode == null) {
+                    throw new NullPointerException("Tried to set null episode");
+                } else if (episode.getSeason() > numSeasons) {
+                    numSeasons = episode.getSeason();
+                }
+            }
+
+            //for loop calculates the max episodes in each season to ensure 2d array doesn't contain null episodes
+            int maxEpisodesInEachSeason[] = new int[numSeasons]; //stores the max number of episodes in each season
+            for (TVEpisode episode : episodes) {
+                if (episode.getNumber() > maxEpisodesInEachSeason[episode.getSeason() - 1]) {
+                    //Changes the number of episodes if the current episode is greater than the length of the season
+                    maxEpisodesInEachSeason[episode.getSeason() - 1] = episode.getNumber();
+                }
+            }
+
+            //array is jagged because different seasons may have different lengths.
+            //Each season should only have a certain amount of episodes.
+            TVEpisode[][] organizedEpisodeArray = new TVEpisode[numSeasons][];
+            for (int season = 0; season < numSeasons; season++) {
+                organizedEpisodeArray[season] = new TVEpisode[maxEpisodesInEachSeason[season]];
+            }
+
+            //Initialize and set each episode of the new 2D array
+            for (TVEpisode episode: episodes) {
+                organizedEpisodeArray[episode.getSeason() - 1 ][episode.getNumber() - 1] = episode;
+            }
+
+            this.episodes = organizedEpisodeArray;
+
+        } else {
+            this.episodes = null;
+        }
+
+    }
+
+    /**
+     * Creates a new TVEpisode for an instance of TVSeries.
+     * Note that this function does not affect the list of episodes in the series. addNewEpisodes() does.
+     * @return a new TVEpisode of the given instance.
+     */
+    public TVEpisode createNewEpisode() {
+        return new TVEpisode();
+    }
+
+    /**
+     * This function will add a new episode to the TVSeries episodes array.
+     * If the episode already exists it will be replaced by the newEpisode
+     * @param newEpisode contains a custom made episode
+     */
+    public void addNewEpisode(TVEpisode newEpisode) {
+
+        //Copy current episodes to a list
+        LinkedList<TVEpisode> currentEpisodeList = new LinkedList<>();
+        for (TVEpisode[] season : episodes) {
+            Collections.addAll(currentEpisodeList, season);
+        }
+
+        //Add the new episode to the list
+        currentEpisodeList.add(newEpisode);
+
+        //Uses the setEpisodes function to reinitialize episodes array
+        TVEpisode[] newEpisodeList = currentEpisodeList.toArray(new TVEpisode[currentEpisodeList.size()]);
+        this.setEpisodes(newEpisodeList);
+
+    }
+
+    public TVEpisode[] getEpisodesInYear(int year) {
+
+        LinkedList<TVEpisode> episodesInYear = new LinkedList<>();
+
+        for (TVEpisode[] season : episodes) {
+            for (TVEpisode episode : season) {
+                if (episode.getAirdate().substring(0, 4).equals(Integer.toString(year))) {
+                    episodesInYear.add(episode);
+                }
+            }
+        }
+
+        return episodesInYear.toArray(new TVEpisode[episodesInYear.size()]);
+
+    }
+
+    public TVEpisode[] getSeasonPremiereEpisodes() {
+
+        TVEpisode[] seasonPremiereEpisodes = new TVEpisode[episodes.length];
+        for (int i = 0; i < seasonPremiereEpisodes.length; i++) {
+            seasonPremiereEpisodes[i] = episodes[i][0];
+        }
+
+        return seasonPremiereEpisodes;
+
+    }
+
+    /**
+     * This class represents the data stored in a TVEpisode within an instance of TVSeries.
+     */
     public class TVEpisode {
 
         private String name;
@@ -141,7 +273,9 @@ public class TVSeries {
         }
 
         public void setNumber(int number) {
-            this.number = number;
+            if (this.getSeason() != 0) {
+                this.number = number;
+            }
         }
 
         public void setAirdate(String airdate) {
@@ -155,12 +289,14 @@ public class TVSeries {
         @Override
         public boolean equals(final Object episode) {
 
+            //A TVEpisode cannot be equal to another object that is not a TVEpisode
             if (!(episode instanceof TVEpisode)) {
                 return false;
             }
 
             TVEpisode otherEpisode = (TVEpisode) episode;
 
+            //Ensures that all fields of the episode are equal to each other.
             return this.name.equals(otherEpisode.getEpisodeName())
                     && this.season == otherEpisode.getSeason()
                     && this.number == otherEpisode.getNumber()
@@ -170,4 +306,5 @@ public class TVSeries {
         }
 
     }
+
 }
--- test/TVSeriesTest.java
+++ test/TVSeriesTest.java
@@ -1,5 +1,3 @@
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.junit.Test;
 import java.util.Arrays;
@@ -8,77 +6,52 @@ import static org.junit.Assert.*;
 
 public class TVSeriesTest {
 
-    //Loads JSON data about the TV Series into form of a TVSeries object. Info retrieved from "Data" folder.
-    private static final TVSeries GAME_OF_THRONES = deserializeData("GameOfThrones");
-    private static final TVSeries HOMELAND = deserializeData("Homeland");
-
-    /**
-     * This class will deserialize the information located in a JSON object to fit the fields of the TVSeries class.
-     * @param tvShow is the name of the TVShow.
-     * @return a TVSeries object with all fields initialized.
-     */
-    private static TVSeries deserializeData(String tvShow) {
-
-        //Stores all the data from JSON file in the form of a Java JsonObject
-        JsonObject tvShowData = new JsonParser().parse(Data.getFileContentsAsString(tvShow)).getAsJsonObject();
-
-        //This will initialize summary, name, and language fields for a TVSeries
-        TVSeries tvSeries = new Gson().fromJson(tvShowData, TVSeries.class);
-
-        //Ratings are set manually since they are located within an object
-        tvSeries.setRatings(tvShowData.get("rating").getAsJsonObject().get("average").getAsDouble());
-
-        //Network name is set manually since it is located within an object
-        tvSeries.setNetworkName(tvShowData.get("network").getAsJsonObject().get("name").getAsString());
-
-        //Premiere date is set manually because the field "premiered" is not specific enough
-        tvSeries.setPremiereDate(tvShowData.get("premiered").getAsString());
-
-        //Since the genres are stored in a JsonArray, we loop through it to convert to a java array
-        String[] genres = new String[tvShowData.get("genres").getAsJsonArray().size()];
-        for (int i = 0; i < genres.length; i++) {
-            genres[i] = tvShowData.get("genres").getAsJsonArray().get(i).getAsString();
-        }
-        tvSeries.setGenres(genres);
-
-        //Since the episodes are stored in a JsonArray, we loop through it to convert it to a java array
-        TVSeries.TVEpisode[] episodes = new TVSeries.TVEpisode[tvShowData.get("_embedded").getAsJsonObject()
-                                                                         .get("episodes").getAsJsonArray().size()];
-        for (int i = 0; i < episodes.length; i++) {
-            //gets each episodes and stores it in a TVEpisode object
-            episodes[i] = new Gson().fromJson(tvShowData.get("_embedded").getAsJsonObject()
-                                                        .get("episodes").getAsJsonArray().get(i),
-                                                        TVSeries.TVEpisode.class);
-        }
-        tvSeries.setEpisodes(episodes);
-
-        return tvSeries;
-
-    }
-
+    private static final TVSeries GAME_OF_THRONES = new TVSeries(
+            new JsonParser().parse(Data.getFileContentsAsString("GameOfThrones")).getAsJsonObject());
+    private static final TVSeries HOMELAND = new TVSeries(
+            new JsonParser().parse(Data.getFileContentsAsString("Homeland")).getAsJsonObject());
 
     @Test
     public void getSeriesNameTest() throws AssertionError {
 
-        assertEquals("Game of Thrones", GAME_OF_THRONES.getName());
+        assertEquals("Game of Thrones", GAME_OF_THRONES.getSeriesName());
 
     }
 
     @Test
     public void getGenreTest() throws AssertionError {
 
-        //Tests for first value in genre array
-        assertTrue(Arrays.asList(HOMELAND.getGenres()).contains("Drama"));
-
-        //Tests for other values in genre array
+        //Tests for random value in genre array
         assertTrue(Arrays.asList(GAME_OF_THRONES.getGenres()).contains("Fantasy") );
+
+    }
+
+    @Test
+    public void setNullEpisode() throws AssertionError {
+
+        boolean exceptionThrown= false;
+        try {
+            TVSeries.TVEpisode[] nullArray = new TVSeries.TVEpisode[1];
+            GAME_OF_THRONES.setEpisodes(nullArray);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        assertTrue("Setting null episodes should throw an exception", exceptionThrown);
 
     }
 
     @Test
     public void getGenreArrayTest() throws AssertionError {
 
+        //Tests all genres in genre array
         assertArrayEquals(new String[] { "Drama", "Thriller", "Espionage" }, HOMELAND.getGenres());
+
+    }
+
+    @Test
+    public void getLanguageTest() throws AssertionError {
+
+        assertEquals("English", GAME_OF_THRONES.getLanguage());
 
     }
 
@@ -93,7 +66,7 @@ public class TVSeriesTest {
     public void getRatingTest() throws AssertionError {
 
         //Delta ensures that any loss of precision is accounted for (this is because of how doubles work).
-        assertEquals(9.3, GAME_OF_THRONES.getRatings(), .01);
+        assertEquals(9.3, GAME_OF_THRONES.getAverageRating(), .01);
 
     }
 
@@ -108,55 +81,60 @@ public class TVSeriesTest {
     public void getSeriesSummaryTest() throws AssertionError {
 
         String homelandSummary = "<p>The winner of 6 Emmy Awards including Outstanding Drama Series, <b>Homeland</b> is an edge-of-your-seat sensation. Marine Sergeant Nicholas Brody is both a decorated hero and a serious threat. CIA officer Carrie Mathison is tops in her field despite being bipolar. The delicate dance these two complex characters perform, built on lies, suspicion, and desire, is at the heart of this gripping, emotional thriller in which nothing short of the fate of our nation is at stake.</p>";
-        assertEquals(homelandSummary, HOMELAND.getSummary());
+        assertEquals(homelandSummary, HOMELAND.getSeriesSummary());
 
     }
 
     @Test
     public void getEpisodeSummaryTest() throws AssertionError {
 
-        //Tests for early cases
         String homelandS1E1Summary = "<p>In the opener of this \"Manchurian Candidate\"-like political thriller, a marine rescued after eight years as a POW in Afghanistan returns home a war hero. But a CIA operative suspects he may actually be an enemy agent with a connection to Al Qaeda and part of a plan to commit a terrorist act on U.S. soil.</p>";
         assertEquals(homelandS1E1Summary, HOMELAND.getEpisode(1, 1).getSummary());
-
-        //Tests for middle cases
-        String gotS5E2Summary = "<p>Arya arrives in Braavos; Brienne and Podrick find danger while traveling; Cersei worries about Myrcella in Dorne when Ellaria Sand seeks revenge for Oberyn's death; Jon is tempted by Stannis.</p>";
-        assertEquals(gotS5E2Summary, GAME_OF_THRONES.getEpisode(5, 2).getSummary());
-
-        //Test for late case
-        String homelandS7E12Summary = "<p>Carrie and Saul's mission doesn't go as planned. Elizabeth Keane fights for her presidency.</p>";
-        assertEquals(homelandS7E12Summary, HOMELAND.getEpisode(7, 12).getSummary());
 
     }
 
     @Test
     public void getEpisodeTest() throws AssertionError {
 
-        //Tests equality of a full TVEpisode object by creating another instance of "Winter is Coming" GOT S1E1
-        TVSeries.TVEpisode gotS1E1 = GAME_OF_THRONES.createNewEpisode();
-        gotS1E1.setName("Winter is Coming");
-        gotS1E1.setSummary("<p>Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage.</p>");
-        gotS1E1.setAirdate("2011-04-17");
-        gotS1E1.setSeason(1);
-        gotS1E1.setNumber(1);
-        assertEquals(gotS1E1, GAME_OF_THRONES.getEpisode(1, 1));
-
-        //Tests for later episode edge cases
         TVSeries.TVEpisode homelandS7E12 = HOMELAND.createNewEpisode();
         homelandS7E12.setName("Paean to the People");
         homelandS7E12.setSummary("<p>Carrie and Saul's mission doesn't go as planned. Elizabeth Keane fights for her presidency.</p>");
         homelandS7E12.setAirdate("2018-04-29");
         homelandS7E12.setSeason(7);
         homelandS7E12.setNumber(12);
+
         assertEquals(homelandS7E12, HOMELAND.getEpisode(7, 12));
 
-        //Checks to make sure getEpisode throws an exception for episodes that don't exist
+    }
+
+    @Test
+    public void getNullSeasonAndEpisodeTest() throws AssertionError {
+
+        //Checks to make sure getEpisode returns null for seasons and episodes that don't exist
         boolean exceptionThrown = false;
+
         try {
-            assertNull("This episode doesn't exist", GAME_OF_THRONES.getEpisode(13, 5));
-        } catch (ArrayIndexOutOfBoundsException e) {
+            GAME_OF_THRONES.getEpisode(13, 5);
+        } catch (Exception e) {
             exceptionThrown = true;
         }
+
+        assertTrue(exceptionThrown);
+
+    }
+
+    @Test
+    public void getNullEpisodeInViableSeasonTest() throws AssertionError {
+
+        //Checks to make sure getEpisode returns null for an episode that doesn't exist, even if the season does
+        boolean exceptionThrown = false;
+
+        try {
+            GAME_OF_THRONES.getEpisode(7, 8);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+
         assertTrue(exceptionThrown);
 
     }
@@ -164,56 +142,89 @@ public class TVSeriesTest {
     @Test
     public void getEpisodeNameTest() throws AssertionError {
 
-        //Test for early season/episode
-        assertEquals("Pilot", HOMELAND.getEpisode(1, 1).getEpisodeName());
-
         //Test for middle values
         assertEquals("Blackwater", GAME_OF_THRONES.getEpisode(2, 9).getEpisodeName());
-
-        //Test for last episode
-        assertEquals("TBA", GAME_OF_THRONES.getEpisode(8, 1).getEpisodeName());
 
     }
 
     @Test
-    public void getEpisodeAirDate() throws AssertionError {
+    public void getEpisodeAirDateTest() throws AssertionError {
 
-        //Test for early episode
-        assertEquals("2011-10-09", HOMELAND.getEpisode(1, 2).getAirdate());
-
-        //Test for middle values
-        assertEquals("2011-12-18", HOMELAND.getEpisode(1, 12).getAirdate());
-
-        //Test for last episode
         assertEquals("2018-04-29", HOMELAND.getEpisode(7, 12).getAirdate());
 
     }
 
     @Test
-    public void getEpisodeSeason() throws AssertionError {
+    public void replaceEpisodeTest() {
 
-        //Test for early episode
-        assertEquals(1, HOMELAND.getEpisode(1, 1).getSeason());
+        TVSeries.TVEpisode gotS8E1 = GAME_OF_THRONES.createNewEpisode();
+        gotS8E1.setName("Still don't know the name");
+        gotS8E1.setSummary("I wish I knew the plot!!!");
+        gotS8E1.setAirdate("2011-04-17");
+        gotS8E1.setSeason(8);
+        gotS8E1.setNumber(1);
 
-        //Test for middle values
-        assertEquals(3, GAME_OF_THRONES.getEpisode(3, 2).getSeason());
-
-        //Test for last values
-        assertEquals(8, GAME_OF_THRONES.getEpisode(8, 1).getSeason());
+        GAME_OF_THRONES.addNewEpisode(gotS8E1);
+        assertEquals(gotS8E1, GAME_OF_THRONES.getEpisode(8, 1));
 
     }
 
     @Test
-    public void getEpisodeNumber() throws AssertionError {
+    public void addNewEpisodeTest() {
 
-        //Test for early values
-        assertEquals(1, HOMELAND.getEpisode(1, 1).getNumber());
+        TVSeries.TVEpisode gotS8E2 = GAME_OF_THRONES.createNewEpisode();
+        gotS8E2.setName("Episode hasn't been announced");
+        gotS8E2.setSummary("I'm no plot leaker :)");
+        gotS8E2.setAirdate("2019-04-21");
+        gotS8E2.setSeason(8);
+        gotS8E2.setNumber(2);
+
+        GAME_OF_THRONES.addNewEpisode(gotS8E2);
+        assertEquals(gotS8E2, GAME_OF_THRONES.getEpisode(8, 2));
+
+    }
+
+    @Test
+    public void getEpisodeSeasonTest() throws AssertionError {
 
         //Test for middle values
-        assertEquals(4, GAME_OF_THRONES.getEpisode(4, 4).getNumber());
+        assertEquals(3, GAME_OF_THRONES.getEpisode(3, 2).getSeason());
 
-        //Test for last values
+    }
+
+    @Test
+    public void getEpisodeNumberTest() throws AssertionError {
+
         assertEquals(7, GAME_OF_THRONES.getEpisode(7, 7).getNumber());
+
+    }
+
+    @Test
+    public void getEpisodeByDateTest() throws AssertionError {
+
+        assertEquals(HOMELAND.getEpisode(1, 12),
+                     HOMELAND.getEpisodeOnDate("2011-12-18"));
+
+    }
+
+    @Test
+    public void getEpisodesInYearTest() throws AssertionError {
+
+        TVSeries.TVEpisode[] gotEpisodesIn2019 = { GAME_OF_THRONES.getEpisode(8 , 1) };
+        assertArrayEquals(gotEpisodesIn2019, GAME_OF_THRONES.getEpisodesInYear(2019));
+
+    }
+
+    @Test
+    public void getSeasonPremiereEpisodesTest() throws AssertionError {
+
+        TVSeries.TVEpisode[] gotSeasonPremieres = new TVSeries.TVEpisode[GAME_OF_THRONES.getEpisodes().length];
+
+        for (int season = 0; season < gotSeasonPremieres.length; season++) {
+            gotSeasonPremieres[season] = GAME_OF_THRONES.getEpisode(season + 1, 1);
+        }
+
+        assertArrayEquals(gotSeasonPremieres, GAME_OF_THRONES.getSeasonPremiereEpisodes());
 
     }
 
